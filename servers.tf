9f5b8b875efc37fdd7d2320fe61e4caa7e23235e
Akr <anilkumareddy9757@gmail.com>
2023-05-12 09:36:04 +0530

    source common

--- servers.tf
+++ servers.tf
@@ -16,6 +16,10 @@ resource "aws_instance" "frontend" {
   }
 }
 
+ouput "frontend" {
+  value= aws_instance.frontend.public_ip
+}
+
 resource "aws_instance" "mongodb" {
   ami           = "ami-0b5a2b5b8f2be4ec2"
   instance_type = "t3.micro"
