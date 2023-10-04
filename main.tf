cde40614ae7767818cac55b0716608cd339eae06
akr9757 <anilkumareddy9757@gmail.com>
2023-10-04 12:35:30 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,19 +1,20 @@
 module "vpc" {
-  source = "git::https://github.com/akr9757/tf-module-vpc.git"
-
-  for_each = var.vpc
+  source = "git::https://github.com/raghudevopsb72/tf-module-vpc.git"
+  for_each   = var.vpc
   cidr_block = each.value["cidr_block"]
-  subnets = each.value["subnets"]
-  tags = local.tags
-  env = var.env
+  subnets    = each.value["subnets"]
+  tags       = local.tags
+  env        = var.env
 }
 
-#module "app" {
-#  source = "git::https://github.com/akr9757/tf-module-app.git"
-#
-#  for_each = var.app
-#  instance_type = each.value["instance_type"]
-#  subnet_id = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
-#}
+output "vpc" {
+  value = module.vpc
+}
 
+module "web" {
+  source = "git::https://github.com/raghudevopsb72/tf-module-app.git"
 
+  for_each      = var.app
+  instance_type = each.value["instance_type"]
+  subnet_id     = element(lookup(lookup(module.vpc, each.value["subnet_name"], null), "subnet_ids", null), 0)
+}
