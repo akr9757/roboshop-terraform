7731ca4640fc0e8429ff1f502c16c1127f64f461
akr9757 <anilkumareddy9757@gmail.com>
2023-10-11 11:44:42 +0530

    terraform

--- main.tf
+++ main.tf
@@ -39,8 +39,10 @@ module "docdb" {
   for_each         = var.docdb
   subnets = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
   allow_db_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_db_cidr"], null), "subnet_cidrs", null)
-
   engine_version             = each.value["engine_version"]
+  instance_count = each.value["instace_count"]
+  instance_class = each.value["instace_class"]
+
   env          = var.env
   tags         = local.tags
   vpc_id       = local.vpc_id
