671c939b7ea51fe0c7c107540b74a773a40d5995
akr9757 <anilkumareddy9757@gmail.com>
2023-10-15 11:24:35 +0530

    terraform

--- main.tf
+++ main.tf
@@ -102,11 +102,11 @@ module "app" {
   max_size         = each.value["max_size"]
   min_size         = each.value["min_size"]
 
-  env          = var.env
-  bastion_cidr = var.bastion_cidr
-  tags         = local.tags
-
   subnet_ids     = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
   vpc_id         = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
   allow_app_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_app_cidr"], null), "subnet_cidrs", null)
+
+  env          = var.env
+  bastion_cidr = var.bastion_cidr
+  tags         = local.tags
 }
